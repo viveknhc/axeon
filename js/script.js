7aa704a0723ab99e449fa0742e032e3a8f6511f6
VIVEK K <35141065+viveknhc@users.noreply.github.com>
2025-03-27 12:36:59 +0000

    27/03/25

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+
+var swiper = new Swiper(".blogSwiper", {
+    slidesPerView: 3.2,
+    spaceBetween: 30,
+    grabCursor: true,
+
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        1024: {
+            slidesPerView: 3.2,
+            spaceBetween: 30
+        }
+    }
+});
+
+
+
+
+
 var swiper = new Swiper(".testimonialSwiper", {
     slidesPerView: 2,
     spaceBetween: 30,
@@ -45,10 +80,10 @@ var swiper = new Swiper(".articleSwiper", {
     spaceBetween: 30,
     grabCursor: true,
     loop: true,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     breakpoints: {
 
         200: {
@@ -85,45 +120,48 @@ var swiper = new Swiper(".articleSwiper", {
 const productSlider = new Swiper(".productSlider", {
     // effect: "fade",
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     on: {
-      slideChange: function () {
-        updateActiveButton(this.activeIndex);
-      },
+        slideChange: function () {
+            updateActiveButton(this.activeIndex);
+        },
     },
-  });
+});
 
-  const buttons = document.querySelectorAll(".category-btn");
+const buttons = document.querySelectorAll(".category-btn");
 
-  buttons.forEach((button) => {
+buttons.forEach((button) => {
     button.addEventListener("click", function () {
-      let index = parseInt(this.dataset.index);
-      productSlider.slideTo(index);
-      updateActiveButton(index);
+        let index = parseInt(this.dataset.index);
+        productSlider.slideTo(index);
+        updateActiveButton(index);
     });
-  });
+});
 
-  function updateActiveButton(index) {
+function updateActiveButton(index) {
     buttons.forEach((btn) => btn.classList.remove("active"));
     buttons[index].classList.add("active");
-  }
+}
 
-  // Set initial active category
-  updateActiveButton(productSlider.activeIndex);
-
-
+// Set initial active category
+updateActiveButton(productSlider.activeIndex);
 
 
 
-  let images = document.querySelectorAll(".product-image img");
-        let currentIndex = 0;
 
-        function fadeImages() {
-            images[currentIndex].classList.remove("active");
-            currentIndex = (currentIndex + 1) % images.length;
-            images[currentIndex].classList.add("active");
-        }
 
-        setInterval(fadeImages, 2000); // Change image every 3 seconds
+let images = document.querySelectorAll(".product-image img");
+let currentIndex = 0;
+
+function fadeImages() {
+    images[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add("active");
+}
+
+setInterval(fadeImages, 2000); // Change image every 3 seconds
+
+
+
